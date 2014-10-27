13c1ec5e50a851594cf1d1fa2d6f338e33d54282
libo_liu <qq644531343@gmail.com>
2014-10-27 16:32:23 +0800

    update

--- SNShareSDK/SNShareSDK/snshare/ShareMonitor/Class/UI/SNShareWindow.m
+++ SNShareSDK/SNShareSDK/snshare/ShareMonitor/Class/UI/SNShareWindow.m
@@ -8,6 +8,12 @@
 
 #import "SNShareWindow.h"
 
+@interface SNShareWindow ()
+
+@property (nonatomic,readwrite) CGRect screenFrame;
+
+@end
+
 @implementation SNShareWindow
 
 static SNShareWindow *instance = nil;
@@ -16,7 +22,24 @@ static SNShareWindow *instance = nil;
     if (instance == nil) {
         @synchronized(instance){
             if (instance == nil) {
-                instance = [[SNShareWindow alloc] init];
+                
+                CGRect screenFrame = [UIScreen mainScreen].bounds;
+                
+                instance = [[SNShareWindow alloc] initWithFrame:screenFrame];
+                instance.screenFrame = screenFrame;
+                
+                instance.windowLevel = UIWindowLevelAlert - 1;
+                [instance makeKeyAndVisible];
+                
+                [[NSNotificationCenter defaultCenter] addObserver:instance selector:@selector(rotation:) name:UIApplicationDidChangeStatusBarOrientationNotification object:nil];
+                
+                instance.backgroundColor = [[UIColor darkGrayColor] colorWithAlphaComponent:0.3];
+                
+                UIButton *btn = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+                btn.frame = CGRectMake(0, 50, 200, 200);
+                [btn setTitle:@"Test" forState:UIControlStateNormal];
+                btn.titleLabel.font = [UIFont systemFontOfSize:48];
+                [instance addSubview:btn];
             }
         }
     }
@@ -33,6 +56,46 @@ static SNShareWindow *instance = nil;
 -(void)dismissWindow
 {
     _shareDelegate = nil;
+}
+
+
+#define degreesToRadians(x) (M_PI*(x)/180.0)
+-(void)rotation:(NSNotification *)noti
+{
+    [UIView beginAnimations:@"rotation" context:nil];
+    [UIView setAnimationDuration:0.25];
+    if ([[noti.userInfo valueForKey:UIApplicationStatusBarOrientationUserInfoKey] intValue] == 4) {
+        [UIView setAnimationDuration:0.5];
+    }
+   
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (orientation == UIInterfaceOrientationPortrait)
+    {
+        self.transform = CGAffineTransformIdentity;
+         self.transform = CGAffineTransformMakeRotation(degreesToRadians(0));
+    }else if(orientation == UIInterfaceOrientationLandscapeLeft)
+    {
+        
+        self.transform = CGAffineTransformMakeRotation(degreesToRadians(-90));
+    }else if(orientation == UIInterfaceOrientationLandscapeRight)
+    {
+        self.transform = CGAffineTransformIdentity;
+        self.transform = CGAffineTransformMakeRotation(degreesToRadians(90));
+    }
+    [self layoutSubviews];
+    
+    [UIView commitAnimations];
+}
+
+-(void)layoutSubviews
+{
+    UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+    if (UIInterfaceOrientationIsLandscape(orientation)) {
+        self.bounds = CGRectMake(0, 0, _screenFrame.size.height, _screenFrame.size.width);
+        NSLog(@"%@",NSStringFromCGRect(self.frame));
+    }else {
+        self.bounds = CGRectMake(0, 0, _screenFrame.size.width, _screenFrame.size.height);
+    }
 }
 
 @end
